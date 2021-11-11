eebae24ae8c341a27a85da331e5fefcdf5f502ac
benzina <benzina.mohamed.ali@hotmail.com>
2021-11-11 12:02:55 +0100

    ~ update items navbar

--- app/src/main/java/com/grossosdevelopopoyos/fuji/MainActivity.kt
+++ app/src/main/java/com/grossosdevelopopoyos/fuji/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_library, R.id.navigation_sources, R.id.navigation_settings
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
